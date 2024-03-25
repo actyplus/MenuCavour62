8eda862271ef7d9078e57db831d5fc383802a3f7
actyplus <77025674+actyplus@users.noreply.github.com>
2024-03-25 13:43:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-fetch('https://script.google.com/macros/s/AKfycbzcm8PDEpDGWT0oBNDHZLAnB7AIH3iLyua0M0PHW-inkPmoUMwY5-nzgqmpLZjtIQ_Fmg/exec')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // Qui puoi manipolare il DOM per inserire i dati nel tuo HTML
-  })
-  .catch(error => console.error('Error:', error));
-
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://script.google.com/macros/s/AKfycbxhenNXGT4JH3DrlVe7JJKvqu_Etg3wlLXJWsAfpKun/exec')
+        .then(response => response.json())
+        .then(data => {
+            const menuContainer = document.getElementById('menu-container');
+            data.forEach((menuItem) => {
+                const itemElement = document.createElement('div');
+                itemElement.className = 'menu-item';
+                itemElement.innerHTML = `
+                    <h3>${menuItem.name}</h3>
+                    <p>${menuItem.description}</p>
+                    <p>Prezzo: ${menuItem.price}</p>
+                `;
+                menuContainer.appendChild(itemElement);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+});
