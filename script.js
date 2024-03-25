d4d47ca2056dcb63e36dbf31a43e7d69efbb93d2
actyplus <77025674+actyplus@users.noreply.github.com>
2024-03-25 13:36:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+fetch('https://script.google.com/macros/s/AKfycbzcm8PDEpDGWT0oBNDHZLAnB7AIH3iLyua0M0PHW-inkPmoUMwY5-nzgqmpLZjtIQ_Fmg/exec')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Qui puoi manipolare il DOM per inserire i dati nel tuo HTML
+  })
+  .catch(error => console.error('Error:', error));
 
